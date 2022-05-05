62210952876e334d7f8e09a422ffd63b63c6d1fe
Descry Off <pierrehugot35@gmail.com>
2022-05-06 01:03:52 +0200

    redirect

--- react_front/src/routes/auth/Login.js
+++ react_front/src/routes/auth/Login.js
@@ -1,21 +1,21 @@
 
 
-const Login = () => {
+const Login = ({Status}) => {
 
     
-
 
     return (
         <div className="d-flex w-100 justify-content-center align-items-center text-center p-3 flex-column">
             <h1>Login</h1>
+            {Status ? <h2>OkayMan</h2> : null}
             <div className="form-group p-5 w-50">
                 <div className="form-floating mb-3">
                   <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" required/>
-                  <label for="floatingInput">Adresse Mail</label>
+                  <label htmlFor="floatingInput">Adresse Mail</label>
                 </div>
                 <div className="form-floating">
                   <input type="password" className="form-control" id="floatingPassword" placeholder="mot de passe" required/>
-                  <label for="floatingPassword">Mot de passe</label>
+                  <label htmlFor="floatingPassword">Mot de passe</label>
                 </div>
                 <button type="submit" className="btn btn-primary m-2">Connexion</button>
               </div>
--- react_front/src/routes/auth/Register.js
+++ react_front/src/routes/auth/Register.js
@@ -1,29 +1,29 @@
 import axios from 'axios';
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
-const registerURL = 'http://127.0.0.1:8000/api/users';
 
-console.log(process.env);
 
 const Register = () => {
-
+  let navigate = useNavigate();
   const [Status, setStatus] = useState();
 
   const SubmitHandler = (e) => {
     e.preventDefault();
-    axios.post(registerURL, {
+    axios.post(`${process.env.REACT_APP_API_URL}users`, {
         name: e.target.username.value,
         email: e.target.email.value,
         password: e.target.password.value,
         password_confirmation: e.target.password_confirmation.value
     }).then((response) => setStatus(response));
+    navigate('/login');
+    
   }
 
     return (
         <div className="d-flex w-100 justify-content-center align-items-center text-center p-3 flex-column">
             <h1>Register</h1>
              <form className="w-50" onSubmit={(e) => SubmitHandler(e)}>
-             <h1>{Status}</h1>
               <div className="form-group p-5 w-100">
                   <div className="form-floating mb-3">
                     <input type="text" className="form-control" id="username" name="username" placeholder="SuperUtilisateur" required/>
